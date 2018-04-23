ba34d9135426be302d9d0ecc61dc9d2ec3f1324e
psull <psull@psull-pc.home>
2018-04-23 16:25:01 -0400

    added functionality to message board

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ document.getElementById('buttonClick').onclick = function(){
   	uname = document.getElementById('uname').value;
   	document.getElementById('login').innerHTML="Welcome, " + uname + "! Go see what you've missed on the message board!";
 }
+
+document.getElementById('button').onclick = function(){
+				var thoughts =document.getElementById('thoughts').value;
+				if(thoughts!="" && uname!=""){
+					var newNode= document.createElement('p');
+					newNode.innerHTML = uname + ": " + thoughts;
+					document.getElementById('content').appendChild(newNode);
+				}
+				else{
+					alert("Please login to post on the message board!");
+				}
+				document.getElementById('frm1').reset();
+			}
+
 });
