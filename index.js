45f1e08b5c6b6af0ee7de7f28ccd998591a45455
psull <psull@psull-pc.home>
2018-04-24 18:34:19 -0400

    fixed style changes, added images

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ var uname = "";
 
 document.getElementById('buttonClick').onclick = function(){  
   	uname = document.getElementById('uname').value;
-  	document.getElementById('login').innerHTML="Welcome, " + uname + "! Go see what you've missed on the message board!";
+  	if(uname==""){
+  		alert('Please enter a valid username.');
+  	}
+  	else{
+  		document.getElementById('login').innerHTML="Welcome, " + uname + "! Go see what you've missed on the message board!";
+  	}
 }
 
 document.getElementById('button').onclick = function(){
 				var thoughts =document.getElementById('thoughts').value;
-				if(thoughts!="" && uname!=""){
+				if(uname==""){
+					alert("Please login to post on the message board!");
+				}
+				else if(thoughts==""){
+					alert('Please enter a response');
+				}				
+				else{
 					var newNode= document.createElement('p');
 					newNode.innerHTML = uname + ": " + thoughts;
 					document.getElementById('content').appendChild(newNode);
-				}
-				else{
-					alert("Please login to post on the message board!");
 				}
 				document.getElementById('frm1').reset();
 			}
